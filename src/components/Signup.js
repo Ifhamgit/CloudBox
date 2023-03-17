a8834fa579ecfd582875d7f77cc09b09e45b4a54
Ifham <ifhamhusain778866@gmail.com>
2023-03-18 03:57:29 +0530

    Further UI updation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -90,31 +90,7 @@ const Signup = (props) => {
 
 
   return (
-    // <div className='mt-3'>
-    //   {/* <h2>Create an account to use CloudBox</h2>
-    //   <form onSubmit={handleSubmit}>
-    // <div className="mb-3">
-    //     <label htmlFor="Name" className="form-label">Name</label>
-    //     <input type="text" className="form-control" id="name"onChange={onchange}  name = "name" aria-describedby="emailHelp"/>
-    // </div>
-    // <div className="mb-3">
-    //     <label htmlFor="email" className="form-label">Email address</label>
-    //     <input type="email" className="form-control" id="email" onChange={onchange} name = "email" aria-describedby="emailHelp"/>
-    //     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-    // </div>
-    // <div className="mb-3">
-    //     <label htmlFor="password" className="form-label">Password</label>
-    //     <input type="password" className="form-control" onChange={onchange} name='password' id="password" minLength={5} required />
-    // </div>
-    // <div className="mb-3">
-    //     <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-    //     <input type="password" className="form-control"onChange={onchange} name='cpassword' id="cpassword" minLength={5} required />
-    //                                                                         {/* This "minLength={5} required" functionality works in "onSubmit" */}
-    // </div>
-  
-    // <button type="submit" className="btn btn-primary">Submit</button>
-    // </form> */}
-
+    
     <div className="login-box">
   <h2>Create an Account</h2>
   <form onSubmit={handleSubmit}>
@@ -146,10 +122,6 @@ const Signup = (props) => {
   </a> 
   </form>
 </div>
-
-    // </div>
-
-    
   )
 }
 
